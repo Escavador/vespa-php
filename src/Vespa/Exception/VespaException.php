8272ba59b737aa353a0030ab9d687a722ead6c03
Icaro Jerry <icarojerry@potelo.com.br>
2019-12-05 15:02:50 -0300

    Fixies log message VEspaException

--- src/Vespa/Exception/VespaException.php
+++ src/Vespa/Exception/VespaException.php
@@ -6,6 +6,6 @@ class VespaException extends \Exception
     public function __construct($message)
     {
         $this->code = 1;
-        $this->message = 'Invalid Vespa Host';
+        $this->message = $message;
     }
 }
