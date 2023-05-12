e7ceb33ee2aec44c4cbda9a516c8ed44fb1b163d
Gabriel Peixoto <gabriel@escavador.com>
2023-05-12 18:38:30 -0300

    Suporte a versão anterior do Laravel

--- src/Migrations/MigrationCreator.php
+++ src/Migrations/MigrationCreator.php
@@ -38,7 +38,7 @@ class MigrationCreator extends IlluminateMigrationCreator
      * @param string $table
      * @return string
      */
-    protected function populateStub($stub, $table)
+    protected function populateStub($name, $stub, $table)
     {
         $stub = str_replace('DummyTable', $table, $stub);
         $stub = str_replace('DummyColumnNameStatus', config('vespa.model_columns.status', ''), $stub);
